9c5a51081d8474ef601216053c854a4e1873b6f1
“NischithIsAFK” <nischith.212@gmail.com>
2024-11-06 08:07:09 +0530

    Reading single nd multiple data

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -14,8 +14,36 @@ public class DemoJdbc {
         String url="jdbc:postgresql://localhost:5432/demo-jdbc";
         String user="postgres";
         String password="root123";
+//        String query="SELECT * from student";
+        String query="INSERT into student values(2,'Ronaldo',77)";
+        //create connection step
 
         Connection con=DriverManager.getConnection(url,user,password);
         System.out.println("connection success");
+        //create statement
+        Statement st= con.createStatement();
+
+        //Execute statement
+        ResultSet res=st.executeQuery(query);
+//        System.out.println(res.next()); // If there is data it will give true or else false
+        //initially pointer will be above the first record. when we do res.next() it will then point at 1st record.
+//        String name=res.getString("sname");
+//        System.out.println("name of student is "+name);
+
+
+        //To get multiple values
+        while(res.next()){
+            System.out.print(res.getInt(1)+" ");
+            System.out.print(res.getString(2)+" ");
+            System.out.println(res.getInt(3));
+        }
+        con.close();
+        System.out.println("Connection closed");
+
+
+
+
+
+
     }
 }
