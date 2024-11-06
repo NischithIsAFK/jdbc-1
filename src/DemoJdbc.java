98418e237097f93499a65644769737c6c29712f5
“NischithIsAFK” <nischith.212@gmail.com>
2024-11-06 08:22:06 +0530

    Final commit, change data using sql query

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -15,16 +15,24 @@ public class DemoJdbc {
         String user="postgres";
         String password="root123";
 //        String query="SELECT * from student";
-        String query="INSERT into student values(2,'Ronaldo',77)";
+        int sid=3;
+        String name="Jack";
+        int marks=49;
+
+
+        String query="INSERT into student values (?,?,?)";
         //create connection step
 
         Connection con=DriverManager.getConnection(url,user,password);
         System.out.println("connection success");
         //create statement
-        Statement st= con.createStatement();
-
+        PreparedStatement st= con.prepareStatement(query);
+        st.setInt(1,sid);
+        st.setString(2,name);
+        st.setInt(3,marks);
+        st.execute();
         //Execute statement
-        ResultSet res=st.executeQuery(query);
+//        ResultSet res=st.executeQuery(query); // Use executeQuery when its read operation
 //        System.out.println(res.next()); // If there is data it will give true or else false
         //initially pointer will be above the first record. when we do res.next() it will then point at 1st record.
 //        String name=res.getString("sname");
@@ -32,12 +40,12 @@ public class DemoJdbc {
 
 
         //To get multiple values
-        while(res.next()){
-            System.out.print(res.getInt(1)+" ");
-            System.out.print(res.getString(2)+" ");
-            System.out.println(res.getInt(3));
-        }
-        con.close();
+//        while(res.next()){
+//            System.out.print(res.getInt(1)+" ");
+//            System.out.print(res.getString(2)+" ");
+//            System.out.println(res.getInt(3));
+//        }
+//        con.close();
         System.out.println("Connection closed");
 
 
